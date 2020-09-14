e3b87acce7b74bfd13706c4373636fb547221f38
James Cathcart <cathcart.jm@gmail.com>
2020-09-14 14:09:27 -0600

    Completed:  - Added SingleChildScrollView() wrapper  - First project in book completed In Progress:
    Notes:

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,39 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.deepPurple,
             ),
             body: Builder(
-                builder: (context) => Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Center(
-                        child: Column(children: [
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text("Hello World Travel",
-                            style: TextStyle(
-                                fontSize: 26,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue[800])),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text("Discover the World",
-                            style: TextStyle(
-                                fontSize: 26,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue[800])),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Image.network(
-                            'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
-                            height: 350),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.all(15),
-                          child: RaisedButton(
-                              child: Text('Contact Us'),
-                              onPressed: () => contactUs(context)))
-                    ]))))));
+                builder: (context) => SingleChildScrollView(
+                    child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Center(
+                            child: Column(children: [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text("Hello World Travel",
+                                style: TextStyle(
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue[800])),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text("Discover the World",
+                                style: TextStyle(
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue[800])),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Image.network(
+                                'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                                height: 350),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(15),
+                              child: RaisedButton(
+                                  child: Text('Contact Us'),
+                                  onPressed: () => contactUs(context)))
+                        ])))))));
 
     return materialApp;
   }
